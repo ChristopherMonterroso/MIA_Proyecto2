36886b948788ae76cde98d430cb139f522dea36c
Christopher Monterroso <chrismonc08@gmail.com>
2023-10-29 18:47:33 -0600

    update

--- server/lib/rep.py
+++ server/lib/rep.py
@@ -75,19 +75,19 @@ class Rep:
                     elif part.type == 'p':
                         graph_content+= Rep.column_rep_primary_mbr(part.type,part.fit,part.start,part.size,part.name,part.status) 
             graph_content+= Rep.footer_rep_mbr()      
-            try:
 
-                src = gv.Source(graph_content,format="png")
-                src.render(path_rep)
-                if os.path.exists(path_rep):
-                    os.remove(path_rep)
-                name = os.path.basename(path_rep)+".png"
-                print(path_rep, name)                        
-                s3 = boto3.client('s3', aws_access_key_id=aws_id, aws_secret_access_key=aws_key)            
-                s3.upload_file(path_rep+".png", bucket_name, name)
-                return "Se ha generado el reporte mbr en la ruta "+str(path_rep)+".png\ny se ha cargado a la carpeta de reportes de la web"
-            except :
-                print("Error")
+
+            src = gv.Source(graph_content,format="png")
+            src.render(path_rep)
+            if os.path.exists(path_rep):
+                os.remove(path_rep)
+            name = os.path.basename(path_rep)+".png"
+            print(path_rep, name)                        
+            s3 = boto3.client('s3', aws_access_key_id=aws_id, aws_secret_access_key=aws_key)            
+            s3.upload_file(path_rep+".png", bucket_name, name)
+            return "Se ha generado el reporte mbr en la ruta "+str(path_rep)+".png\ny se ha cargado a la carpeta de reportes de la web"
+
+
 
 
     def obtain_name_disk(path):
